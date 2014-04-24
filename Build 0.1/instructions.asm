b190c4f847832d45b86157a928d716ca0f392bad
dgolynskiy <dgolynskiy@yahoo.com>
2014-04-24 17:38:18 -0500

    Merge branch 'master' of https://github.com/Machine-Coders/Jumbline
    Conflicts:
    Build 0.1/instructions.asm

--- Build 0.1/instructions.asm
+++ Build 0.1/instructions.asm
@@ -1,20 +1,32 @@
 #Intructions Screen
 
+.include "utilityMacros.asm"
+.include "letGenMacros.asm"
+.include "data.asm"
 
 .data
 
-Instructions: .ascii "GOAL OF GAME: \n"
+I_Instructions: .ascii "GOAL OF GAME: \n"
 .ascii "Purpose of this game is to guess all possible words \nfrom the combination of randomly generated letters\n\n"
 .ascii "INSTRUCTIONS: \n"
 .ascii "1. Choose how many characters to generate (5-7).\n"
 .ascii "2. Enter word guesses.\n"
 .asciiz "3. Win by guessing all possible words.\n"
-Null2: .asciiz ""
+I_Null2: .asciiz ""
 
 .text
 li $v0, 59
-la $a0, Instructions
-la $a1, Null2
+la $a0, I_Instructions
+la $a1, I_Null2
 syscall
 
-.include "Let Gen Biased.asm"
+<<<<<<< HEAD
+
+letGen #Macro for gen
+
+
+
+
+=======
+#.include "Let Gen Biased.asm"
+>>>>>>> FETCH_HEAD
